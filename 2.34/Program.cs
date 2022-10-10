4fe25fd3526c7edf1e301436b8669775b1c73254
Сhert <hert@AKIRIN>
2022-10-10 16:22:09 +0300

    Добавьте файлы проекта.

---
+++ 2.34/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+        int num1 = ((A % 10) + B) % 10;
+        int num2 = (A / 10) + ((A % 10) + B) / 10;
+        Console.WriteLine("Десятки: {0}, Единицы: {1}, ", num2, num1);
+    }
+}
